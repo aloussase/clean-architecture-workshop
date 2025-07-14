d383eee36118729e45e8219ce5ce33b0a679ac81
Alexander Goussas <goussasalexander@gmail.com>
2025-07-13 23:50:40 -0500

    init

---
+++ src/test/java/io/github/com/cleanarchworkshop/CleanarchworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.cleanarchworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
